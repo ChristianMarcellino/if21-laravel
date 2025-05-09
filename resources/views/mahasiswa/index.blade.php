1a364c374b214a8250f6e861c669166eb85ea041
ChristianMarcellino <christianmarcellino14@gmail.com>
2025-05-09 20:32:40 +0700

    template

--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -1,38 +1,92 @@
-<a href="mahasiswa">Mahasiswa</a>
-<a href="fakultas">Fakultas</a>
-<a href="prodi">Prodi</a>
-<h1>Mahasiswa</h1>
-<table border="1">
-    <thead>
-        <tr>
-            <th>Nama</th>
-            <th>NPM</th>
-            <th>Jenis Kelamin</th>
-            <th>Asal Sekolah</th>
-            <th>Tempat Lahir</th>
-            <th>Program Studi</th>
-            <th>Fakultas</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($mahasiswa as $item)  
-            <tr>
-                <td>{{ $item -> nama }}</td>
-                <td>{{ $item -> npm }}</td>
-                <td>
-                    <?php 
-                        if($item->jk == 'L'){
-                            echo "Laki Laki";
-                        }else{
-                            echo "Perempuan";
-                        }
-                    ?>
-                </td>
-                <td>{{ $item -> asal_sma }}</td>
-                <td>{{ $item -> tempat_lahir }}</td>
-                <td>{{$item->prodi->nama}}</td>
-                <td>{{ $item -> prodi -> fakultas -> nama }}</td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
+@extends('main')
+
+@section('title', 'Mahasiswa')
+@section('content')
+<div class="app-content">
+          <!--begin::Container-->
+          <div class="container-fluid">
+            <!--begin::Row-->
+            <div class="row">
+              <div class="col-12">
+                <!-- Default box -->
+                <div class="card">
+                  <div class="card-header">
+                    <h3 class="card-title">Mahasiswa</h3>
+                    <div class="card-tools">
+                      <button
+                        type="button"
+                        class="btn btn-tool"
+                        data-lte-toggle="card-collapse"
+                        title="Collapse"
+                      >
+                        <i data-lte-icon="expand" class="bi bi-plus-lg"></i>
+                        <i data-lte-icon="collapse" class="bi bi-dash-lg"></i>
+                      </button>
+                      <button
+                        type="button"
+                        class="btn btn-tool"
+                        data-lte-toggle="card-remove"
+                        title="Remove"
+                      >
+                        <i class="bi bi-x-lg"></i>
+                      </button>
+                    </div>
+                  </div>
+                  <div class="card-body">
+                    
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Nama</th>
+                                <th>NPM</th>
+                                <th>Jenis Kelamin</th>
+                                <th>Asal Sekolah</th>
+                                <th>Tempat Lahir</th>
+                                <th>Program Studi</th>
+                                <th>Fakultas</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($mahasiswa as $item)  
+                                <tr>
+                                    <td>{{ $item -> nama }}</td>
+                                    <td>{{ $item -> npm }}</td>
+                                    <td>
+                                        <?php 
+                                            if($item->jk == 'L'){
+                                                echo "Laki Laki";
+                                            }else{
+                                                echo "Perempuan";
+                                            }
+                                        ?>
+                                    </td>
+                                    <td>{{ $item -> asal_sma }}</td>
+                                    <td>{{ $item -> tempat_lahir }}</td>
+                                    <td>{{$item->prodi->nama}}</td>
+                                    <td>{{ $item -> prodi -> fakultas -> nama }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    <div class="card-footer clearfix">
+                    <ul class="pagination pagination-sm m-0 float-end">
+                      <li class="page-item"><a class="page-link" href="#">&laquo;</a></li>
+                      <li class="page-item"><a class="page-link" href="#">1</a></li>
+                      <li class="page-item"><a class="page-link" href="#">2</a></li>
+                      <li class="page-item"><a class="page-link" href="#">3</a></li>
+                      <li class="page-item"><a class="page-link" href="#">&raquo;</a></li>
+                    </ul>
+                  </div>
+                  </div>
+                  <!-- /.card-body -->
+                  <div class="card-footer">Footer</div>
+                  <!-- /.card-footer-->
+                </div>
+                <!-- /.card -->
+              </div>
+            </div>
+            <!--end::Row-->
+          </div>
+        </div>
+@endsection
+
--- resources/views/prodi/index.blade.php
+++ resources/views/prodi/index.blade.php
@@ -1,13 +1,80 @@
-<a href="mahasiswa">Mahasiswa</a>
-<a href="fakultas">Fakultas</a>
-<a href="prodi">Prodi</a>
+@extends('main')
 
-<h1>Prodi</h1>
+@section('title', 'Program Studi')
+@section('content')
+<div class="app-content">
+          <!--begin::Container-->
+          <div class="container-fluid">
+            <!--begin::Row-->
+            <div class="row">
+              <div class="col-12">
+                <!-- Default box -->
+                <div class="card">
+                  <div class="card-header">
+                    <h3 class="card-title">Program Studi</h3>
+                    <div class="card-tools">
+                      <button
+                        type="button"
+                        class="btn btn-tool"
+                        data-lte-toggle="card-collapse"
+                        title="Collapse"
+                      >
+                        <i data-lte-icon="expand" class="bi bi-plus-lg"></i>
+                        <i data-lte-icon="collapse" class="bi bi-dash-lg"></i>
+                      </button>
+                      <button
+                        type="button"
+                        class="btn btn-tool"
+                        data-lte-toggle="card-remove"
+                        title="Remove"
+                      >
+                        <i class="bi bi-x-lg"></i>
+                      </button>
+                    </div>
+                  </div>
+                  <div class="card-body">
+                    
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Nama</th>
+                                <th>Singkatan</th>
+                                <th>Nama Kaprodi</th>
+                                <th>Nama Sekretaris</th>
+                                <th>Nama Fakultas</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($prodi as $item)  
+                                <tr>
+                                    <td>{{ $item -> nama }}</td>
+                                    <td>{{ $item -> singkatan }}</td>
+                                    <td>{{$item->kaprodi}}</td>
+                                    <td>{{ $item -> sekretaris }}</td>
+                                    <td>{{ $item -> fakultas -> nama}}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    <div class="card-footer clearfix">
+                    <ul class="pagination pagination-sm m-0 float-end">
+                      <li class="page-item"><a class="page-link" href="#">&laquo;</a></li>
+                      <li class="page-item"><a class="page-link" href="#">1</a></li>
+                      <li class="page-item"><a class="page-link" href="#">2</a></li>
+                      <li class="page-item"><a class="page-link" href="#">3</a></li>
+                      <li class="page-item"><a class="page-link" href="#">&raquo;</a></li>
+                    </ul>
+                  </div>
+                  </div>
+                  <!-- /.card-body -->
+                  <div class="card-footer">Footer</div>
+                  <!-- /.card-footer-->
+                </div>
+                <!-- /.card -->
+              </div>
+            </div>
+            <!--end::Row-->
+          </div>
+        </div>
+@endsection
 
-@foreach ($prodi as $item)
-<li>{{ $item -> nama }}</li>
-<li>{{ $item -> singkatan }}</li>
-<li>{{ $item -> kaprodi }}</li>
-<li>{{ $item -> sekretaris }}</li>
-<li>{{$item->fakultas->nama}}</li>
-@endforeach
