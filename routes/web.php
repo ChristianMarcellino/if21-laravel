70da0f4b050a2e87c11d5bdce651bf83353b93ae
ChristianMarcellino <christianmarcellino14@gmail.com>
2025-06-10 17:35:12 +0700

    asdsad

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,28 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FakultasController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\ProdiController;
-use App\Http\Controllers\MataKuliahController;
-use App\Http\Controllers\SesiController;
-use App\Http\Controllers\JadwalController;
-use Illuminate\Support\Facades\Route;
 
-Route::get('/', action: function () {
+Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('fakultas', FakultasController::class)->parameters([
-    'fakultas' => 'fakultas'
-]);
-Route::resource('prodi', ProdiController::class);
-Route::resource('mahasiswa', MahasiswaController::class);
-Route::resource('mata_kuliah', MataKuliahController::class);
-Route::resource('sesi', SesiController::class);
-Route::resource('jadwal', JadwalController::class);
-Route::get('dashboard', [DashboardController::class, 'index']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('/fakultas', FakultasController::class);
+    Route::resource('/prodi', ProdiController::class);
+    Route::resource('/mahasiswa', MahasiswaController::class);
+});
+
+
+
+require __DIR__.'/auth.php';
